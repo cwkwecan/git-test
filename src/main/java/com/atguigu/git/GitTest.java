bf459086ddca903d1f8a925e041ce85c1cfc64ef
cwk <cwkwecan@163.com>
2022-10-06 17:24:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author cwk
+ * @create 2022-10-05 23:33
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
